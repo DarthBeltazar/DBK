34b3359a4ed5c17f57c6f003ada7dc67b9badfb9
AlexandrGeorgiev <geosasha.mail@gmail.com>
2026-06-03 22:06:27 +0300

    Initial commit

---
+++ src/main/java/com/example/addon/modules/EnoughLight.java
@@ -0,0 +1,146 @@
+package com.example.addon.modules;
+
+import com.example.addon.AddonTemplate;
+import meteordevelopment.meteorclient.events.render.Render3DEvent;
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.renderer.ShapeMode;
+import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.render.color.Color;
+import meteordevelopment.meteorclient.utils.render.color.SettingColor;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.LightType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnoughLight extends Module {
+    private final SettingGroup sgGeneral = this.settings.getDefaultGroup();
+    private final SettingGroup sgRender = this.settings.createGroup("Render");
+
+
+    private final Setting<Integer> scanRadius = sgGeneral.add(new IntSetting.Builder()
+        .name("scan-radius")
+        .description("Scan radius (horizontally)")
+        .min(1)
+        .max(128)
+        .sliderMax(64)
+        .defaultValue(32)
+        .build()
+    );
+
+    private final Setting<Integer> scanHeight = sgGeneral.add(new IntSetting.Builder()
+        .name("scan-height")
+        .description("Scan radius (vertically)")
+        .min(1)
+        .max(128)
+        .sliderMax(64)
+        .defaultValue(32)
+        .build()
+    );
+
+    private final Setting<Integer> updRate = sgGeneral.add(new IntSetting.Builder()
+        .name("update-rate")
+        .description("Pause between updates")
+        .min(1)
+        .max(60)
+        .sliderMax(60)
+        .defaultValue(10)
+        .build()
+    );
+
+    private final Setting<Boolean> checkAir = sgGeneral.add(new BoolSetting.Builder()
+    .name("check-air")
+    .defaultValue(true)
+    .build()
+    );
+
+
+
+    private final Setting<SettingColor> fColor = sgRender.add(new ColorSetting.Builder()
+        .name("fill-color")
+        .description("The color of the marker.")
+        .defaultValue(new Color(255, 10, 10, 15))
+        .build()
+    );
+
+    private final Setting<SettingColor> eColor = sgRender.add(new ColorSetting.Builder()
+        .name("edge-color")
+        .description("The color of the marker.")
+        .defaultValue(new Color(255, 10, 10, 200))
+        .build()
+    );
+
+    private final Setting<ShapeMode> shapeMode = sgRender.add(new EnumSetting.Builder<ShapeMode>()
+        .name("shapemode")
+        .defaultValue(ShapeMode.Both)
+        .build()
+    );
+
+
+    public EnoughLight() {
+        super(AddonTemplate.DBK, "enough-light", "An module, which highlights mob spawn places");
+    }
+
+
+    private List<BlockPos> spawnBlocks = new ArrayList<BlockPos>();
+    private int timer = 0;
+
+    @Override
+    public void onActivate() {
+        spawnBlocks.clear();
+        timer = 0;
+    }
+
+    @Override
+    public void onDeactivate() {
+        spawnBlocks.clear();
+    }
+
+    private void updateBlockPos(){
+        spawnBlocks.clear();
+        int r = scanRadius.get();
+        int h = scanHeight.get();
+        for(BlockPos pos : BlockPos.iterateOutwards(mc.player.getBlockPos(), r, h, r)){
+            if(!mc.world.isInBuildLimit(pos)){
+                continue;
+            }
+            if(!mc.world.isAir(pos) || !mc.world.getBlockState(pos.down()).isSolidBlock(mc.world, pos.down())){
+                continue;
+            }
+            if(checkAir.get()){
+                if (!mc.world.isAir(pos.up())){
+                    continue;
+                }
+            }
+            if(mc.world.getLightLevel(LightType.BLOCK, pos) > 0){
+                continue;
+            }
+
+            spawnBlocks.add(pos.toImmutable());
+        }
+    }
+
+    @EventHandler
+    private void onTick(TickEvent.Post event) {
+        if (mc.player == null || mc.world == null) return;
+        if (timer > updRate.get()) {
+            updateBlockPos();
+            timer = 0;
+        }
+        timer++;
+    }
+
+    @EventHandler
+    private void onRender3d(Render3DEvent event) {
+        if(spawnBlocks.isEmpty()) return;
+        for (BlockPos pos : spawnBlocks) {
+            event.renderer.box(pos, fColor.get(), eColor.get(), shapeMode.get(), 0);
+        }
+
+    }
+
+
+}
